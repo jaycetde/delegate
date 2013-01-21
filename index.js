919931abbd9e929654d9c9d357b9150cebbcbef4
ForbesLindesay <forbes@lindesay.co.uk>
2013-01-21 14:25:34 +0000

    delegate with children

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ var matches = require('matches-selector')
   , event = require('event');
 
 /**
+ * Look for an element that is a child of top
+ * and a parent of top, that matches selector.
+ *
+ * @param {Element} top the parent node in which to search
+ * @param {Element} bottom the starting place for the search
+ * @param {String} selector a css query used to determine if a node matches
+ * @return {Element|null}
+ * @api private
+ */
+function findMatch(top, bottom, selector) {
+  while (bottom != top && bottom) {
+    if (matches(bottom, selector)) return bottom;
+    bottom = bottom.parentElement;
+  }
+  return null;
+}
+
+/**
  * Delegate event `type` to `selector`
  * and invoke `fn(e)`. A callback function
  * is returned which may be passed to `.unbind()`.
@@ -22,7 +40,7 @@ var matches = require('matches-selector')
 
 exports.bind = function(el, selector, type, fn, capture){
   return event.bind(el, type, function(e){
-    if (matches(e.target, selector)) fn(e);
+    if (e.delegateTarget = findMatch(el, e.target, selector)) fn(e);
   }, capture);
   return callback;
 };
